3390d4f890c176010d13b4581ce507df10776b9c
Arold Mauger <aroldmauger2022@gmail.com>
2023-05-27 21:35:38 +0200

    Ajout du code frontend et du code de base backend du cours

---
+++ backend/routes/stuff.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+
+
+router.post('/ok', auth, multer, stuffCtrl.createThing);
+
+
+module.exports = router;
+
